6a371df1b92e3bfd668177ceb8cc3428c0cdb7fd
JudisP <judisson429@hotmail.com>
2023-12-29 15:36:38 -0300

    StyCode - Separando as rotas do server.js

--- src/server.js
+++ src/server.js
@@ -4,9 +4,99 @@ const express = require("express");
 const app = require("./config/express");
 const PORT = process.env.PORT || 3332;
 
-const Chat = require("./db/models/chat.model");
+// const testMessage = true;
+
+//---------------------------------------------------------------------------------
+//Mensagem teste
+
+// async function handleTestChat(testMessage) {
+//   let testeDMessage = testMessage;
+//   console.log("entrou na função");
+//   if (testMessage) {
+//     console.log("Entrou no if", testeDMessage); 
+//     let chatId = "190598850804966_556196985714";
+//     let chat = await Chat.findOne({ chatId: chatId });
+
+//     console.log("char se existerrrr", chat);
+
+//     const chatTesteMe = new Chat({
+//       messages: [
+//         {
+//           messageId: "messageId",
+//           from: "15550636210",
+//           to: "5561996985714",
+//           timestamp: new Date(),
+//           text: {
+//             body: "Olá, mundo!1",
+//           },
+//           type: "text",
+//           contact_name: "João",
+//           contact_number: "5561996985714",
+//           fields: "algum campo",
+//         },
+//       ],
+//     });
+
+//     console.log("ChattesteMe: ", chatTesteMe.messages );
+
+//     if (!chat) {
+//       console.log("entrando no chat novo");
+//       let newChat = new Chat({
+//         chatId,
+//         messages: chatTesteMe.messages,
+//         // messages: messages.map((message) => ({
+//         //   // messageId: message.id,
+//         //   from: message.from,
+//         //   to: message.to,
+//         //   timestamp: message.timestamp || new Date(),
+//         //   body: message.body,
+//         //   type: message.type,
+//         //   contact_name: message.contact_name,
+//         //   contact_number: message.contact_number,
+//         // //   statuses: message.statuses ? message.statuses.map(status => ({
+//         // //     idStatus: status.idStatus,
+//         // //     status: status.status,
+//         // //     timestamp: status.timestamp || new Date(), // Adiciona a data e hora atual se timestamp não for fornecido
+//         // //     toNumber: status.toNumber
+//         // // })) : [],
+//         // fields: message.fields
+//         // })),
+//       });
+//       // console.log("vai Puxar a mensagem");
+//       // newChat.messages.push(messages);
+
+//       // console.log("newChat", newChat); 
+//       await newChat
+//         .save()
+//         .then((doc) => console.log("Documento salvo novo: ", doc)) 
+//         .catch((err) => console.error("Erro ao salvar o documento:", err));
+//     } else {
+//       // console.log("chat antigos");
+        
+//       console.log("entrando no chat existente ", chat );     
+//       chat.messages.push(...chatTesteMe.messages);
+
+//       await chat
+//         .save()
+//         .then((doc) => console.log("Documento salvo:", doc))
+//         .catch((err) => console.error("Erro ao salvar o documento:", err));
+//     }
+//   }
+// }
+// async function iniciarAplicacao() {
+//   try {
+//     await connectMongoDB();
+//     handleTestChat(testMessage).then(() => console.log("Processo de Chat teste Concluiído")).catch((err) => console.error("Erro ao fazer o chat teste: ", err));
+//   } catch (err) {
+//     console.error("Erro");
+//   }
+// }
 
 connectMongoDB();
+
+// iniciarAplicacao();
+
+//---------------------------------------------------------------------------------
 
 // const chatTeste = new Chat({
 //   chatId: "chat12345",
@@ -35,112 +125,10 @@ connectMongoDB();
 
 // chatTeste
 //   .save()
-  // .then((doc) => console.log("Documento salvo:", doc))
-  // .catch((err) => console.error("Erro ao salvar o documento:", err));
+//   .then((doc) => console.log("Documento salvo:", doc))
+//   .catch((err) => console.error("Erro ao salvar o documento:", err));
 
 app.use(express.json());
-
-app.post("/mensagens-recebidas-data", async (req, res) => {
-  console.log("Dados recebidos: ");
-  const formattedJson = JSON.stringify(req.body, null, 2);
-  console.log(formattedJson);
-  console.log("\n");
-  console.log(
-    "--------------------------------------------------------------------------------------"
-  );
-  console.log("\n");
-  // Aqui você vai tratar os dados recebidos e depois armazená-los no banco de dados
-
-  try {
-    const entries = req.body.entry || [];
-
-    await Promise.all(
-      entries.map(async (entry) => {
-        const idNumberMe = entry.changes[0].value.metadata.phone_number_id;
-        const contacts = entry.changes[0].value.contacts;
-        // console.log("contato: \n", contacts);
-        const messages = entry.changes[0].value.messages;
-        // .map(message => {
-        //   let newMessage = {...message}; // Cria uma cópia do objeto message
-        //   // newMessage.externalId = newMessage.id; // Renomeia 'id' para 'externalId'
-        //   delete newMessage.id; // Remove o campo 'id' original
-        //   return newMessage;
-        // });
-
-        // messages2 = messages.map(message => {
-        //   let newMessage = {...message}; // Cria uma cópia do objeto message
-        //   // newMessage.externalId = newMessage.id; // Renomeia 'id' para 'externalId'
-        //   delete newMessage.id; // Remove o campo 'id' original
-        //   return newMessage;
-        // });
-        // console.log("mensagens: ", messages2.map((message) => ({
-        //   message
-        // })));
-        console.log("mensagem: ", messages);
-        console.log("mensagem test: \n");
-
-        contacts.map(async (contact) => {
-          const idNumberClient = contact.wa_id;
-          const chatId = `${idNumberMe}_${idNumberClient}`;
-
-          let chat = await Chat.findOne({ chatId: chatId });
-
-          if (!chat) {
-            let newChat = new Chat({
-              chatId,
-              messages: messages,
-              // messages: messages.map((message) => ({
-              //   // messageId: message.id,
-              //   from: message.from,
-              //   to: message.to,
-              //   timestamp: message.timestamp || new Date(),
-              //   body: message.body,
-              //   type: message.type,
-              //   contact_name: message.contact_name,
-              //   contact_number: message.contact_number,
-              // //   statuses: message.statuses ? message.statuses.map(status => ({
-              // //     idStatus: status.idStatus,
-              // //     status: status.status,
-              // //     timestamp: status.timestamp || new Date(), // Adiciona a data e hora atual se timestamp não for fornecido
-              // //     toNumber: status.toNumber
-              // // })) : [],
-              // fields: message.fields
-              // })),
-            });
-            console.log("vai Puxar a mensagem");
-            // newChat.messages.push(messages);
-
-            console.log('newChat', newChat)
-            await newChat.save().then((doc) => console.log("Documento salvo novo: ", doc))
-            .catch((err) => console.error("Erro ao salvar o documento:", err));
-          } else {
-            chat.messages.push(...messages);
-
-            await chat.save().then((doc) => console.log("Documento salvo : ", doc))
-            .catch((err) => console.error("Erro ao salvar o documento existente: ", err));
-          }
-
-          // if (chat) {
-
-          // }
-        });
-
-        // messages.map(async (message) => {
-        //   const idNumberClient = message.from;
-        // });
-      })
-    );
-
-    res.status(200).send("Dados recebidos com sucesso!");
-  } catch (error) {
-    console.log("Erro ao inserir os dados", error);
-  }
-  // res.status(200).send('U | Dados recebidos com sucesso!');
-});
-
-app.get("/", (req, res) => {
-  res.send("Servidor rodando!");
-});
 
 // process.on("uncaughtException", (err) => {
 //   console.error("Exceção não capturada:", err);
@@ -150,3 +138,5 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   // console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+// module.exports = handleTestChat;
