025fd9436d0d2f03531b4acdd0142a1f5c25f8a0
JudisDev <judisson429@hotmail.com>
2023-12-31 14:59:18 -0300

    Feat - Criando o Controller

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,9 +1,24 @@
-const { receiveMessage } = require('../class/instance')
+const { receiveMessage, statusMessage, sendMessageClient } = require('../class/instance')
 const Chat = require('../db/models/chat.model');
 
 exports.Receive = async (req, res) => {
     const data = await receiveMessage(
-        req.body.entry || []
+        req.body || []
     )
-    return res.status(200).json({ data: data})
+    return res.status(200).json({ data: data })
+}
+
+exports.Status = async (req, res) => {
+    const data = await statusMessage(
+        req.body || []
+    )
+    return res.status(200).json({ data: data })
+}
+
+exports.Response = async (req, res) => {
+    const data = await sendMessageClient(
+        req.body.resposta
+    )
+    console.log("Data que vem do ServidorGPT: ", data)
+    return res.status(200).json(data)
 }
