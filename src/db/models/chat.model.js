c8057a337cc2f97b1073f511678c7411e8de7de1
JudisDev <judisson429@hotmail.com>
2023-12-31 14:59:47 -0300

    Feat - Mudanças no Schema

--- src/db/models/chat.model.js
+++ src/db/models/chat.model.js
@@ -1,41 +1,42 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 statusSchema = new mongoose.Schema({
-    idStatus: String,
-    status: String,
-    timestamp: Date,
-    toNumber: String,
-    conversation: {
-        id: String,
-        origin: {
-        type: String
-        }
+  messageId: String,
+  status: String,
+  timestamp: Date,
+  toNumber: Number,
+  conversation: {
+    id: String,
+    expiration_timestamp: String,
+    origin: {
+      type: { type: String},
     },
-    pricing: {
-        billable: Boolean,
-        pricing_model: String,
-        category: String
-    }
-})
+  },
+  pricing: {
+    billable: Boolean,
+    pricing_model: String,
+    category: String,
+  },
+});
 
 const messageSchema = new mongoose.Schema({
-    messageId: String, //id dentro de messages.message
-    from: String,
-    to: String,
-    timestamp: Date,
-    text: { body: String },
-    type: String,
-    contact_name: String, //name
-    contact_number: String, //wa_id
-    statuses: [statusSchema],
-    fields: String
-})
+  messageId: String, //id dentro de messages.message
+  from: String,
+  to: String,
+  timestamp: Date,
+  text: { body: String },
+  type: String,
+  contact_name: String, //name
+  contact_number: String, //wa_id
+  statuses: [statusSchema],
+  fields: String,
+});
 
 const chatSchema = new mongoose.Schema({
-    chatId: String,
-    messages: [messageSchema]
-})
+  chatId: String,
+  messages: [messageSchema],
+});
 
-const Chat = mongoose.model('chat', chatSchema)
+const Chat = mongoose.model('chat', chatSchema);
 
-module.exports = Chat
+module.exports = Chat;
